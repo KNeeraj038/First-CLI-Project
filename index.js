045d8bf404d950c985065a5a7e6f45fab5f8340f
KNeeraj038 <>
2021-04-24 06:36:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var readlineSync = require('readline-sync');
+
+console.log("")
+
+var userName = readlineSync.question("Let's start with your name : ");
+
+console.log("Hi "+userName+", good to see you !");
+
